aafd6896c668fe25e5145fae3e78b57bc6b3088f
adrianboon-cmd <sinhan95@gmail.com>
2026-09-16 11:19:11 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/plain; charset=utf-8"
+  });
+
+  res.end("Visit Report Bot Running");
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
