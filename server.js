82bba738c45973d374836838923184f362da5645
adrianboon-cmd <sinhan95@gmail.com>
2026-09-16 11:21:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/plain; charset=utf-8"
-  });
+const server = http.createServer((req,res)=>{
 
-  res.end("Visit Report Bot Running");
+ if(req.url==="/"){
+
+   const html = fs.readFileSync(
+      "visit-form.html",
+      "utf8"
+   );
+
+   res.writeHead(200,{
+      "Content-Type":"text/html;charset=utf-8"
+   });
+
+   res.end(html);
+
+   return;
+ }
+
+ res.writeHead(404);
+ res.end("Not Found");
+
 });
 
 const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+server.listen(port,()=>{
+ console.log(`Server running on ${port}`);
 });
